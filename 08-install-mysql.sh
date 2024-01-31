c533457815d51b2a9d65999a62cd40675166b3d0
iamnr <avuthunaveenreddy@gmail.com>
2024-01-31 11:27:27 +0530

    First commit

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -7,3 +7,13 @@ then
     echo "Run script with root access"
     exit 1
 fi 
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Instalation if mysql is in erro"
+    exit 1
+else
+    echo "Sucess"
+fi 
